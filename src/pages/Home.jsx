b11cca229e768663c9379a4779b10da58f3d7c87
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-04-23 14:04:33 +0200

    responsive style home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="h-screen bg-[#FCF5EB]">Home</div>;
+  return (
+    <div className="h-screen flex flex-col md:flex-row bg-blue-500">
+      <div className="md:w-1/3 relative bg-slate-300 flex justify-center items-center md:justify-start md:items-start">
+        <img
+          src="./../src/assets/img/laptopChat.png"
+          alt="laptop"
+          className="object-contain h-full hidden md:block"
+        />
+        <img
+          src="./../src/assets/img/iphonePhone.png"
+          alt="iphone"
+          className="h-[400px]  rotate-45 absolute md:hidden top-8 left-16"
+        />
+        <img
+          src="./../src/assets/img/pixelPhone.png"
+          alt="iphone"
+          className="h-[450px] absolute md:hidden left-4 top-8 "
+        />
+      </div>
+      <div className="bg-transparent border-2 border-red-600 flex-1"></div>
+    </div>
+  );
 };
 
 export default Home;
